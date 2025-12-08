a3fcde5c7caca35872d1ab136980fd2be6f3d03d
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2025-12-08 16:56:14 +0530

    here i have used only springDataJpa for crud opertions with database

---
+++ springdatajpa/src/main/java/com/abhishekvermaa10/exception/PetNotFoundException.java
@@ -0,0 +1,12 @@
+package com.abhishekvermaa10.exception;
+
+
+public class PetNotFoundException extends Exception {
+	  
+	private static final long serialVersionUID = 1L; 
+
+	public PetNotFoundException(String message) {
+		super(message);
+	}
+	 
+}
---
+++ springdatajpa/src/main/java/com/abhishekvermaa10/repository/OwnerRepository.java
@@ -0,0 +1,13 @@
+package com.abhishekvermaa10.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abhishekvermaa10.entity.Owner;
+
+
+public interface OwnerRepository extends JpaRepository<Owner, Integer>{
+	
+}
